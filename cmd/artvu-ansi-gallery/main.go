b51fb01c9113b7eb6d5c6eabc720d8536b61d0ff
robbiew <robbiew@gmail.com>
2021-06-13 15:11:06 -0700

    header adjust

--- cmd/artvu-ansi-gallery/main.go
+++ cmd/artvu-ansi-gallery/main.go
@@ -2,40 +2,35 @@ package main
 
 import (
 	"fmt"
-	"log"
-	"os"
 
+	"github.com/BurntSushi/toml"
 	"github.com/robbiew/artvu-ansi-gallery/internal/ansi"
 	"github.com/robbiew/artvu-ansi-gallery/internal/menu"
 	"github.com/robbiew/artvu-ansi-gallery/pkg/term"
 )
 
-func main() {
+type Config struct {
+	HeaderHeight int
+	ThemeDir     string
+	ArtDir       string
+}
 
-	var headerH int
+func main() {
 
 	var h int
 	var w int
-	var rootDir string
 
-	headerH = 4
+	var conf Config
+	if _, err := toml.DecodeFile("/robbiew/artvu-ansi-gallery/cfg/config.toml", &conf); err != nil {
+		// handle error
+		if err != nil {
+			panic(err)
+		}
+	}
 
-	if len(os.Args) == 1 {
-		log.Fatal("Please specify path, or .")
-		return
-	}
-	if rootDir = os.Args[1]; rootDir == "" {
-		log.Fatal("Please specify path, or .")
-		return
-	}
-	if _, err := os.Stat(rootDir); os.IsNotExist(err) {
-		// path/to/whatever does not exist
-		log.Fatal("Path does not exist!")
-		return
-	}
-	if _, err := os.Stat(rootDir); !os.IsNotExist(err) {
-		// path/to/whatever exists, continue
-	}
+	headerH := conf.HeaderHeight
+	rootDir := conf.ArtDir
+	themeDir := conf.ThemeDir
 
 	// Try and detect the user's term size
 	h, w = term.GetTermSize()
@@ -43,5 +38,5 @@ func main() {
 	fmt.Println(ansi.Clear)
 	fmt.Println(ansi.Home)
 
-	menu.MenuAction(rootDir, h, w, headerH)
+	menu.MenuAction(rootDir, h, w, headerH, themeDir)
 }
